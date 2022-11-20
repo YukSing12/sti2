a8f96d137f46b0c94a28575d12509ea31db169e8
YukSing12 <8216396+YukSing12@user.noreply.gitee.com>
2022-11-20 03:20:46 +0000

    Refactoring test dir

--- test/plugins/testAddLayerNormPluginOnnx.py
+++ test/plugins/testAddLayerNormPluginOnnx.py
@@ -23,9 +23,10 @@ import onnx
 import onnx_graphsurgeon as gs
 import numpy as np
 
-soFilePath      = './so/plugins/libLayerNormPlugin.so'
-nBS             = 1
-nSL             = 128
+soFilePath      = './AddLayerNormPlugin.so'
+nBS             = 7
+# nH              = 50
+nW              = 128
 nEmbedding      = 768
 epsilon         = 1e-5
 npDataType      = np.float32
@@ -40,7 +41,11 @@ def check(a, b, weak = False):
         return np.all( a == b )
 
 def layerNormCPU(bufferH):
-    _x = bufferH[0]
+    _x1 = bufferH[0]
+    _x2 = bufferH[1]
+    _x3 = bufferH[2]
+    _x = _x1 + _x2 + _x3
+    # nEmbed = bufferH[0].shape[3]
     _0  = np.mean(_x,2)[:,:,np.newaxis]
     _1  = _x - _0
     _2  = _1 * _1
@@ -55,17 +60,20 @@ def layerNormCPU(bufferH):
 
 def getLayerNormOnnx():
     onnx_file = "temp.onnx"
-    shape = ('B', nSL, nEmbedding)
-    x = gs.Variable(name="x", dtype=npDataType, shape=shape)
+    shape = ('B', nW, nEmbedding)
+    x1 = gs.Variable(name="x1", dtype=npDataType, shape=shape)
+    x2 = gs.Variable(name="x2", dtype=npDataType, shape=shape)
+    x3 = gs.Variable(name="x3", dtype=npDataType, shape=shape)
+
     gamma = gs.Constant(name="gamma", values=globalGamma)
     beta = gs.Constant(name="beta", values=globalBeta)
     y = gs.Variable(name="y", dtype=npDataType, shape=shape)
     layernorm = gs.Node(op="LayerNorm", 
                         name="LayerNorm_1", 
-                        inputs=[x, gamma, beta], 
+                        inputs=[x1, x2, x3, gamma, beta], 
                         outputs=[y], 
                         attrs={"epsilon":epsilon})
-    graph = gs.Graph(nodes=[layernorm], inputs=[x], outputs=[y])
+    graph = gs.Graph(nodes=[layernorm], inputs=[x1, x2, x3], outputs=[y])
     onnx.save(gs.export_onnx(graph), onnx_file)
     return onnx_file
 
@@ -95,16 +103,28 @@ def run():
             exit()
         print("Succeeded parsing onnx file!")
     
-    inputTensor = network.get_input(0)  # x
-    print("inputTensor.name:{}".format(inputTensor.name))
-    profile.set_shape(inputTensor.name, (1, nSL, nEmbedding), (1, nSL, nEmbedding), (1, nSL, nEmbedding))  
+    inputTensor1 = network.get_input(0)  # x
+    inputTensor2 = network.get_input(1)  # x
+    inputTensor3 = network.get_input(2)  # x
+
+
+    print("inputTensor.name:{}".format(inputTensor1.name))
+    print("inputTensor.name:{}".format(inputTensor2.name))
+    print("inputTensor.name:{}".format(inputTensor3.name))
+    profile.set_shape(inputTensor1.name, (1, nW, nEmbedding), (4, nW, nEmbedding), (10, nW, nEmbedding))  
+    profile.set_shape(inputTensor2.name, (1, nW, nEmbedding), (4, nW, nEmbedding), (10, nW, nEmbedding))  
+    profile.set_shape(inputTensor3.name, (1, nW, nEmbedding), (4, nW, nEmbedding), (10, nW, nEmbedding))  
+
     config.add_optimization_profile(profile)
 
     engineString = builder.build_serialized_network(network, config)
     engine = trt.Runtime(logger).deserialize_cuda_engine(engineString)
 
     context = engine.create_execution_context()
-    context.set_binding_shape(0,[nBS,nSL,nEmbedding])
+    context.set_binding_shape(0,[nBS,nW,nEmbedding])
+    context.set_binding_shape(1,[nBS,nW,nEmbedding])
+    context.set_binding_shape(2,[nBS,nW,nEmbedding])
+
     print("Binding all? %s"%(["No","Yes"][int(context.all_binding_shapes_specified)]))
     
     nInput = np.sum([ engine.binding_is_input(i) for i in range(engine.num_bindings) ])
@@ -113,8 +133,11 @@ def run():
         print("input ->" if engine.binding_is_input(i) else "output->",engine.get_binding_dtype(i),engine.get_binding_shape(i),context.get_binding_shape(i))
 
     bufferH = []
-    bufferH.append( np.random.rand(nBS,nSL,nEmbedding).astype(npDataType).reshape(nBS,nSL,nEmbedding) * 2 - 1)
-    bufferH.append(np.empty(context.get_binding_shape(1),dtype=trt.nptype(engine.get_binding_dtype(1))))
+    bufferH.append( np.random.rand(nBS,nW,nEmbedding).astype(npDataType).reshape(nBS,nW,nEmbedding) * 2 - 1)
+    bufferH.append( np.random.rand(nBS,nW,nEmbedding).astype(npDataType).reshape(nBS,nW,nEmbedding) * 2 - 1)
+    bufferH.append( np.random.rand(nBS,nW,nEmbedding).astype(npDataType).reshape(nBS,nW,nEmbedding) * 2 - 1)
+
+    bufferH.append(np.empty(context.get_binding_shape(3),dtype=trt.nptype(engine.get_binding_dtype(3))))
 
     bufferD = []
     for i in range(engine.num_bindings):
@@ -130,9 +153,7 @@ def run():
 
     print("check result:")
     temp1 = bufferH[-1]
-    temp2 = layerNormCPU(bufferH[:1])
-    print(temp1)
-    print(temp2)
+    temp2 = layerNormCPU(bufferH[:3])
     print(check(temp1,temp2,True), "max diff=%f"%(np.max(np.abs(temp1 - temp2))) )
     
     for b in bufferD:
---
+++ test/test.sh
@@ -0,0 +1,21 @@
+ROOT_DIR=$(cd $(dirname $0); pwd)
+
+echo "========================== Evaluate performance of model ========================"
+/usr/src/tensorrt/bin/trtexec \
+    --loadEngine=Ernie.plan \
+    --avgRuns=1000 \
+    --profilingVerbosity=detailed \
+    --minShapes=read_file_0.tmp_0:1x128x1,read_file_0.tmp_1:1x128x1,read_file_0.tmp_2:1x128x1,read_file_0.tmp_3:1x128x1,read_file_0.tmp_6:1x1x1,read_file_0.tmp_7:1x1x1,read_file_0.tmp_8:1x1x1,read_file_0.tmp_9:1x1x1,read_file_0.tmp_10:1x1x1,read_file_0.tmp_11:1x1x1,read_file_0.tmp_12:1x1x1,read_file_0.tmp_13:1x1x1 \
+    --optShapes=read_file_0.tmp_0:4x128x1,read_file_0.tmp_1:4x128x1,read_file_0.tmp_2:4x128x1,read_file_0.tmp_3:4x128x1,read_file_0.tmp_6:4x1x1,read_file_0.tmp_7:4x1x1,read_file_0.tmp_8:4x1x1,read_file_0.tmp_9:4x1x1,read_file_0.tmp_10:4x1x1,read_file_0.tmp_11:4x1x1,read_file_0.tmp_12:4x1x1,read_file_0.tmp_13:4x1x1 \
+    --maxShapes=read_file_0.tmp_0:10x128x1,read_file_0.tmp_1:10x128x1,read_file_0.tmp_2:10x128x1,read_file_0.tmp_3:10x128x1,read_file_0.tmp_6:10x1x1,read_file_0.tmp_7:10x1x1,read_file_0.tmp_8:10x1x1,read_file_0.tmp_9:10x1x1,read_file_0.tmp_10:10x1x1,read_file_0.tmp_11:10x1x1,read_file_0.tmp_12:10x1x1,read_file_0.tmp_13:10x1x1 \
+    --plugins=./so/plugins/LayerNormPlugin.so
+    # --plugins=./AddVBiasTransposePlugin.so \
+    # --plugins=./LayerNormPlugin.so \
+    # --plugins=./Mask2PosPlugin.so \
+    # --plugins=./MaskedSoftmaxPlugin.so
+
+echo "========================== Evaluate accuracy of model ========================"
+./bin/main.exe ./Ernie.plan ./data/label.test.txt ./label.res.txt ./so/plugins
+./bin/main.exe ./Ernie.plan ./data/perf.test.txt ./perf.res.txt ./so/plugins
+python src/utils/local_evaluate.py ./label.res.txt
+python src/utils/local_evaluate.py ./perf.res.txt
