45cd66fe08833817c53fc7ac715b18b1d92f6ffb
YukSing12 <8216396+YukSing12@user.noreply.gitee.com>
2022-11-29 03:45:44 +0000

    update run.sh with multiprofile
    Signed-off-by: YukSing12 <8216396+YukSing12@user.noreply.gitee.com>

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ export LD_PRELOAD="$ROOT_DIR/so/tensorrt/lib/libnvinfer_builder_resource.so.8.5.
 
 if [ $1 = "data/perf.test.txt" ]; then
     rm  ./perf.res.txt
-    ./bin/main ./model/Ernie.plan ./data/perf.test.txt ./perf.res.txt ./so/plugins/
+    ./bin/multiprofile ./model/Ernie.plan ./data/perf.test.txt ./perf.res.txt ./so/plugins/
 else
      rm  ./label.res.txt
-    ./bin/main ./model/Ernie.plan ./data/label.test.txt ./label.res.txt ./so/plugins/
+    ./bin/multiprofile ./model/Ernie.plan ./data/label.test.txt ./label.res.txt ./so/plugins/
 fi
