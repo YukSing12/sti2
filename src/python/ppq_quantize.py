e02ddbe16d6b598ad72543e3e193e97e89980283
YukSing12 <8216396+YukSing12@user.noreply.gitee.com>
2022-12-16 04:44:10 +0000

    !67 从量化模型导出ft所需的scale_list * Export scale list from quantized model * Use per-channel quantization * Add FasterTransformerQuantizer

--- src/python/ppq_quantize.py
+++ src/python/ppq_quantize.py
@@ -2,13 +2,19 @@ from typing import Iterable, Tuple
 
 import torch
 import numpy as np
-from ppq import (BaseGraph, QuantizationSettingFactory, TargetPlatform,
-                 convert_any_to_numpy, torch_snr_error)
-from ppq.api import (dispatch_graph, export_ppq_graph, load_onnx_graph,
-                     quantize_onnx_model)
-from ppq.core.data import convert_any_to_torch_tensor
-from ppq.executor.torch import TorchExecutor
-from ppq.quantization.analyse.graphwise import graphwise_error_analyse
+import ppq.lib as PFL
+from ppq import BaseQuantizer, Operation, OperationQuantizationConfig, TargetPlatform, TorchExecutor, graphwise_error_analyse, BaseGraph, QuantableOperation
+from ppq.api import ENABLE_CUDA_KERNEL, load_onnx_graph, register_network_quantizer
+from ppq.core import (PASSIVE_OPERATIONS, QuantizationPolicy, QuantizationProperty,
+                      QuantizationStates, RoundingPolicy, convert_any_to_numpy)
+from ppq.IR import BaseGraph, Operation, QuantableOperation, SearchableGraph
+from ppq.lib.quant import Exporter
+from ppq.quantization.optim import (LayerwiseEqualizationPass,
+                                    LearnedStepSizePass, ParameterQuantizePass,
+                                    PassiveParameterQuantizePass,
+                                    QuantAlignmentPass, QuantizeFusionPass,
+                                    QuantizeSimplifyPass,
+                                    RuntimeCalibrationPass)
 
 BATCHSIZE = 1
 SEQ_LEN = 128
@@ -30,6 +36,37 @@ QUANT_PLATFORM = TargetPlatform.TRT_INT8
 ONNX_PATH = 'model/model_dymshape_13.onnx'
 ONNX_OUTPUT_PATH = 'model/qt_model_dymshape.onnx'
 CAL_DATASET = 'data/label.test.txt'
+
+from ppq.quantization.optim import QuantizationOptimizationPass
+class FuseQKVOptimPass(QuantizationOptimizationPass):
+    def __init__(self, name: str = 'FuseQKV Optimization Pass') -> None:
+        super().__init__(name)
+    def optimize(self, graph: BaseGraph, **kwargs) -> None:
+        search_engine = SearchableGraph(graph=graph)
+        matches = search_engine.pattern_matching(
+            patterns=[lambda x: True, 'Flatten', 'MatMul', 'Flatten', 'MatMul', 'Flatten', 'MatMul'],
+            edges=[[0, 1], [1, 2], [0, 3], [3, 4], [0, 5], [5, 6]], exclusive=False)
+        
+        visited = set()
+        for any, _, mat1, _, mat2, _, mat3 in matches:
+            if any in visited: continue
+            visited.add(any)
+            
+            assert isinstance(mat1, QuantableOperation)
+            assert isinstance(mat2, QuantableOperation)
+            assert isinstance(mat3, QuantableOperation)
+            
+            input_TQC1 = mat1.config.input_quantization_config[0]
+            input_TQC2 = mat2.config.input_quantization_config[0]
+            input_TQC3 = mat3.config.input_quantization_config[0]
+            
+            # 你可以在这里将 input_TQC1.scale 设置成三者中最大的，不过这个操作一般对精度也没啥特别重要的影响
+            # 下面的语句将建立 input_TQC1, input_TQC2, input_TQC3 三者的相互关系
+            # input_TQC2, input_TQC3 将与 input_TQC1 共用 scale
+            input_TQC2.master_by = input_TQC1
+            input_TQC3.master_by = input_TQC1
+
+            print(f'{mat1.name}, {mat2.name}, {mat3.name} has been fused.')
 
 # ------------------------------------------------------------
 # 在这个例子中我们将向你展示如何量化一个 onnx 模型，执行误差分析，并与 onnxruntime 对齐结果
@@ -57,81 +94,118 @@ def generate_calibration_dataset(graph: BaseGraph, num_of_batches: int = 32) -> 
                 sample.update({name: value})
                 i += 1
             dataset.append(sample)
-    return dataset, sample  # last sample
+    return dataset
 
 
 def collate_fn(batch: dict) -> torch.Tensor:
     return {k: v.to(DEVICE) for k, v in batch.items()}
 
 
-# ------------------------------------------------------------
-# 在这里，我们仍然创建一个 QuantizationSetting 对象用来管理量化过程
-# 我们将调度方法修改为 conservative，并且要求 PPQ 启动量化微调
-# ------------------------------------------------------------
-QSetting = QuantizationSettingFactory.default_setting()
-QSetting.lsq_optimization = True
-
-# ------------------------------------------------------------
-# 准备好 QuantizationSetting 后，我们加载模型，并且要求 ppq 按照规则完成图调度
-# ------------------------------------------------------------
 graph = load_onnx_graph(onnx_import_file=ONNX_PATH)
-graph = dispatch_graph(graph=graph, platform=QUANT_PLATFORM, setting=QSetting)
-for name in graph.inputs:
-    if name not in INPUTS:
-        raise KeyError(f'Graph Input {name} needs a valid shape.')
+
+class FasterTransformerQuantizer(BaseQuantizer):
+    # ------------------------------------------------------------
+    # quant_operation_types 是一个类型枚举，在这里你需要写下所有该量化器所需要量化的算子
+    # ------------------------------------------------------------
+    @ property
+    def quant_operation_types(self) -> set:
+        return {'MatMul'}
+
+    # ------------------------------------------------------------
+    # 一旦你确定了那些算子需要量化，则需要在 init_quantize_config 为他们初始化量化信息
+    # 然而你需要注意的是，由于手动调度的存在，用户可以强制调度一个类型不在 quant_operation_types 中的算子来到量化平台
+    # 我建议你针对这类情况进行回应。或者，在探测到算子类型并非可量化类型后进行报错
+    # ------------------------------------------------------------
+    def init_quantize_config(self, operation: Operation) -> OperationQuantizationConfig:
+        # ------------------------------------------------------------
+        # 为矩阵乘算子初始化量化信息，只量化矩阵乘算子的输出
+        # ------------------------------------------------------------
+        base_quant_config = self.create_default_quant_config(
+            policy=self.quantize_policy, rounding=self.rounding_policy,
+            op=operation, num_of_bits=8, exponent_bits=0,
+            quant_max=127, quant_min=-128,
+            observer_algorithm='percentile'
+        )
+        if operation.type == 'MatMul':
+            if operation.inputs[1].is_parameter:
+                gemm_weight_config = base_quant_config.input_quantization_config[1]
+                gemm_weight_config.policy = QuantizationPolicy(
+                    QuantizationProperty.SYMMETRICAL +
+                    QuantizationProperty.LINEAR +
+                    QuantizationProperty.PER_CHANNEL
+                )
+                gemm_weight_config.channel_axis = 0
+                gemm_weight_config.observer_algorithm = 'minmax'
+            
+        if operation.type in PASSIVE_OPERATIONS:
+            # Those op are not active op.
+            base_quant_config.is_active_quant_op = False
+        return base_quant_config
+
+    @ property
+    def quantize_policy(self) -> QuantizationPolicy:
+        return QuantizationPolicy(
+            QuantizationProperty.SYMMETRICAL +
+            QuantizationProperty.LINEAR +
+            QuantizationProperty.PER_TENSOR
+        )
+
+    @ property
+    def target_platform(self) -> TargetPlatform:
+        return TargetPlatform.EXTENSION
+
+    @ property
+    def rounding_policy(self) -> RoundingPolicy:
+        return RoundingPolicy.ROUND_HALF_EVEN
+
+    @ property
+    def activation_fusion_types(self) -> set:
+        return {}
+register_network_quantizer(FasterTransformerQuantizer, TargetPlatform.EXTENSION)
 
 # ------------------------------------------------------------
-# 生成校准所需的数据集，我们准备开始完成网络量化任务
+# 我们首先进行标准的量化流程，为所有算子初始化量化信息，并进行 Calibration
 # ------------------------------------------------------------
-calibration_dataset, sample = generate_calibration_dataset(graph)
-quantized = quantize_onnx_model(
-    onnx_import_file=ONNX_PATH, calib_dataloader=calibration_dataset,
-    calib_steps=32, input_shape=None, inputs=collate_fn(sample),
-    setting=QSetting, collate_fn=collate_fn, platform=QUANT_PLATFORM,
-    device=DEVICE, verbose=0)
+
+quantizer   = FasterTransformerQuantizer(graph)
+dispatching = PFL.Dispatcher(graph=graph).dispatch(          # 生成调度表
+    quant_types=quantizer.quant_operation_types)
+
+# 为算子初始化量化信息
+for op in graph.operations.values():
+    quantizer.quantize_operation(
+        op_name = op.name, platform = dispatching[op.name])
+
+# 初始化执行器
+executor = TorchExecutor(graph=graph, device=DEVICE)
+calibration_dataset = generate_calibration_dataset(graph)
+executor.tracing_operation_meta(inputs=calibration_dataset[0])
+executor.load_graph(graph=graph)
 
 # ------------------------------------------------------------
-# 在 PPQ 完成网络量化之后，我们特别地保存一下 PPQ 网络执行的结果
-# 在本样例的最后，我们将对比 PPQ 与 Onnxruntime 的执行结果是否相同
+# 创建优化管线，由于后续还要继续训练我们的模型，我们不能在此处调用
+# ParameterBakingPass()，一旦模型权重完成烘焙，则它们不能被进一步调整
 # ------------------------------------------------------------
-executor, reference_outputs = TorchExecutor(quantized), []
-for sample in calibration_dataset:
-    reference_outputs.append(executor.forward(collate_fn(sample)))
+pipeline = PFL.Pipeline([
+    QuantizeSimplifyPass(),
+    QuantizeFusionPass(
+        activation_type=quantizer.activation_fusion_types),
+    ParameterQuantizePass(),
+    RuntimeCalibrationPass(),
+    PassiveParameterQuantizePass(),
+    QuantAlignmentPass(force_overlap=True),
+    FuseQKVOptimPass()
+])
 
-# ------------------------------------------------------------
-# 执行网络误差分析，并导出计算图
-# ------------------------------------------------------------
-graphwise_error_analyse(
-    graph=quantized, running_device=DEVICE,
-    collate_fn=collate_fn, dataloader=calibration_dataset)
-export_ppq_graph(graph=quantized, platform=TargetPlatform.ONNXRUNTIME,
-                 graph_save_to=ONNX_OUTPUT_PATH)
+with ENABLE_CUDA_KERNEL():
+    # 调用管线完成量化
+    pipeline.optimize(
+        graph=graph, dataloader=calibration_dataset, verbose=True, 
+        calib_steps=32, collate_fn=collate_fn, executor=executor)
 
-# -----------------------------------------
-# 在最后，我们启动 onnxruntime 并比对结果
-# -----------------------------------------
-try:
-    import onnxruntime
-except ImportError as e:
-    raise Exception('Onnxruntime is not installed.')
+    graphwise_error_analyse(
+        graph=graph, running_device=DEVICE, dataloader=calibration_dataset,
+        collate_fn=collate_fn, steps=8)
 
-sess = onnxruntime.InferenceSession(
-    ONNX_OUTPUT_PATH, providers=['CUDAExecutionProvider'])
-onnxruntime_outputs = []
-for sample in calibration_dataset:
-    onnxruntime_outputs.append(sess.run(
-        output_names=[name for name in graph.outputs],
-        input_feed={k: v.numpy() for k, v in sample.items()}))
-
-name_of_output = [name for name in graph.outputs]
-for oidx, output in enumerate(name_of_output):
-    y_pred, y_real = [], []
-    for reference_output, onnxruntime_output in zip(reference_outputs, onnxruntime_outputs):
-        y_pred.append(convert_any_to_torch_tensor(
-            reference_output[oidx], device='cpu').unsqueeze(0))
-        y_real.append(convert_any_to_torch_tensor(
-            onnxruntime_output[oidx], device='cpu').unsqueeze(0))
-    y_pred = torch.cat(y_pred, dim=0)
-    y_real = torch.cat(y_real, dim=0)
-    print(
-        f'Simulating Error For {output}: {torch_snr_error(y_pred=y_pred, y_real=y_real).item() :.4f}')
+    exporter = Exporter(platform=TargetPlatform.ONNXRUNTIME)
+    exporter.export(file_path=ONNX_OUTPUT_PATH, graph=graph)
--- src/python/qtonnx2bin.py
+++ src/python/qtonnx2bin.py
@@ -6,6 +6,11 @@ import onnx_graphsurgeon as gs
 import numpy as np
 import configparser
 
+ACTIVATION_AMAX_NUM = 72
+INT8O_GEMM_NUM = 8
+TRT_AMAX_NUM = 3
+SCALE_RESERVE_NUM = 21
+HIDDEN_DIM = 768
 
 def get_args():
     parser = argparse.ArgumentParser('Export Weights of Ernie', add_help=False)
@@ -17,35 +22,226 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def dump_dequantize_node(node, rst):
-    name = node.inputs[0].name.replace("w_0_0", "w_0")
-    print("Export {}".format(name))
-    rst[name] = node.inputs[0].values
-    name = node.inputs[0].name.replace("w_0_0", "w_0_scales")
-    print("Export {}".format(name))
-    rst[name] = node.inputs[1].values
+def fill_part1_amax(scale_list, amax):
+    scale_list[0] = amax
+    scale_list[1] = amax / 127.0
+    scale_list[2] = amax / 127.0 / 127.0
+    scale_list[3] = 127.0 / amax
+    return scale_list
 
-def dump_weights(nodes, nodes_dict):
+def dump_scales(nodes, nodes_dict):
     rst = dict()
     layer_num = 0
     for node in nodes:
-        if node.op == "Add" and len(node.outputs[0].outputs) == 7:
+        if node.op == "Add" and len(node.outputs[0].outputs) == 4:
+            name = "encoder_layer_{}_scale_list".format(layer_num)
+            q_flatten = nodes_dict["p2o.Flatten.{}".format(0 + layer_num * 12)]
+            k_flatten = nodes_dict["p2o.Flatten.{}".format(2 + layer_num * 12)]
+            v_flatten = nodes_dict["p2o.Flatten.{}".format(4 + layer_num * 12)]
+
             q_mm = nodes_dict["p2o.MatMul.{}".format(2 + layer_num * 16)]
             k_mm = nodes_dict["p2o.MatMul.{}".format(4 + layer_num * 16)]
             v_mm = nodes_dict["p2o.MatMul.{}".format(6 + layer_num * 16)]
+
+            q_bias = nodes_dict["p2o.Add.{}".format(10 + layer_num * 26)]
+            k_bias = nodes_dict["p2o.Add.{}".format(12 + layer_num * 26)]
+            v_bias = nodes_dict["p2o.Add.{}".format(14 + layer_num * 26)]
+
+            bmm1 = nodes_dict["p2o.MatMul.{}".format(8 + layer_num * 16)]
+            softmax = nodes_dict["p2o.Softmax.{}".format(0 + layer_num * 1)]
+            bmm2 = nodes_dict["p2o.MatMul.{}".format(10 + layer_num * 16)]
+
             proj_mm = nodes_dict["p2o.MatMul.{}".format(12 + layer_num * 16)]
-            ffn1_mm = nodes_dict["p2o.MatMul.{}".format(14 + layer_num * 16)]
-            ffn2_mm = nodes_dict["p2o.MatMul.{}".format(16 + layer_num * 16)]
+            proj_bias_norm = nodes_dict["p2o.Add.{}".format(24 + layer_num * 26)]
+
+            fc1_mm = nodes_dict["p2o.MatMul.{}".format(14 + layer_num * 16)]
+            fc1_bias = nodes_dict["p2o.Add.{}".format(26 + layer_num * 26)]
+            fc2_mm = nodes_dict["p2o.MatMul.{}".format(16 + layer_num * 16)]
+            fc2_bias_norm = nodes_dict["p2o.Add.{}".format(34 + layer_num * 26)]
+
+            size = ACTIVATION_AMAX_NUM + 9 * HIDDEN_DIM + INT8O_GEMM_NUM + TRT_AMAX_NUM
+            scale_list = np.zeros(size)
+
+            # Part 1 -- 72:
+            #   First 72 are for activation amaxs. For each activation amax, there are 4 values: amax, amax/127.0f,
+            #   amax/127.0f/127.0f, 127.0f/amax -- input_amax 0-3 , Q_aftergemm_amax 4-7, Qbias_amax 8-11, K_aftergemm_amax
+            #   12-15, Kbias_amax 16-19, V_aftergemm_amax 20-23, Vbias_amax 24-27, bmm1_amax 28-31, Softmax_amax 32-35,
+            #   bmm2_amax 36-39, Proj_aftergemm_scale 40-43, ProjBiasNorm_amax 44-47, FC1_aftergemm_amax 48-51, F1Bias_amax
+            #   52-55, FC2_aftergemm_amax 56-59, F2BiasNorm_amax 60-63, reserve 64-71
             
-            dump_dequantize_node(q_mm.inputs[1].inputs[0], rst)
-            dump_dequantize_node(k_mm.inputs[1].inputs[0], rst)
-            dump_dequantize_node(v_mm.inputs[1].inputs[0], rst)
-            dump_dequantize_node(proj_mm.inputs[1].inputs[0], rst)
-            dump_dequantize_node(ffn1_mm.inputs[1].inputs[0], rst)
-            dump_dequantize_node(ffn2_mm.inputs[1].inputs[0], rst)
+            # input_amax
+            input_amax = q_flatten.outputs[0].outputs[0].inputs[1].values * 127
+            idx = 0
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], input_amax)
+
+            # Q_aftergemm_amax
+            q_aftergemm_amax = q_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], q_aftergemm_amax)
+
+            # Qbias_amax
+            q_bias_amax = q_bias.outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], q_bias_amax)
+
+            # K_aftergemm_amax
+            k_aftergemm_amax = k_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], k_aftergemm_amax)
+
+            # Kbias_amax
+            k_bias_amax = k_bias.outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], k_bias_amax)
             
+            # V_aftergemm_amax
+            v_aftergemm_amax = v_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], v_aftergemm_amax)
+
+            # Vbias_amax
+            v_bias_amax = v_bias.outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], v_bias_amax)
+
+            # bmm1_amax
+            bmm1_amax = bmm1.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], bmm1_amax)
+
+            # Softmax_amax
+            softmax_amax = softmax.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], softmax_amax)
+
+            # bmm2_amax
+            bmm2_amax = bmm2.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], bmm2_amax)
+
+            # Proj_aftergemm_scale
+            proj_aftergemm_amax = proj_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], proj_aftergemm_amax)
+
+            # ProjBiasNorm_amax
+            proj_bias_norm_amax = proj_bias_norm.outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], proj_bias_norm_amax)
+
+            # FC1_aftergemm_amax
+            fc1_amax = fc1_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], fc1_amax)
+
+            # F1Bias_amax
+            fc1_bias_amax = fc1_bias.outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], fc1_bias_amax)
+
+            # FC2_aftergemm_amax
+            fc2_amax = fc2_mm.outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], fc2_amax)
+
+            # F2BiasNorm_amax
+            if(layer_num == 11):    # A little different in the finaly layer
+                fc2_bias_norm_amax = fc2_bias_norm.outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            else:
+                fc2_bias_norm_amax = fc2_bias_norm.outputs[0].outputs[0].outputs[0].outputs[0].inputs[1].values * 127
+            idx += 1
+            fill_part1_amax(scale_list[idx * 4: idx * 4 + 4], fc2_bias_norm_amax)
+
+            # Part 2 -- 9*hidden_dim:
+            #   Kernel amaxs, for each kernel amax list, there are output_channel values : query_weight_amax_list,
+            #   key_weight_amax_list, value_weight_amax_list, proj_weight_amax_list, FC1_weight_amax_list, FC2_weight_amax_list
+            p2_offset = ACTIVATION_AMAX_NUM
+
+            # query_weight_amax_list
+            query_weight_amax = q_mm.inputs[1].inputs[0].inputs[1].values
+            idx = 0
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM] = query_weight_amax
+
+            # key_weight_amax_list
+            key_weight_amax = k_mm.inputs[1].inputs[0].inputs[1].values
+            idx += 1
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM] = key_weight_amax
+
+            # value_weight_amax_list
+            value_weight_amax = v_mm.inputs[1].inputs[0].inputs[1].values
+            idx += 1
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM] = value_weight_amax
+
+            # proj_weight_amax_list
+            proj_weight_amax = proj_mm.inputs[1].inputs[0].inputs[1].values
+            idx += 1
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM] = proj_weight_amax
+
+            # fc1_weight_amax_list
+            fc1_weight_amax = fc1_mm.inputs[1].inputs[0].inputs[1].values
+            idx += 1
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM] = fc1_weight_amax
+
+            # fc2_weight_amax_list (4 x HIDDEN_DIM)
+            fc2_weight_amax = fc2_mm.inputs[1].inputs[0].inputs[1].values
+            idx += 1
+            scale_list[p2_offset + idx * HIDDEN_DIM: p2_offset + idx * HIDDEN_DIM + HIDDEN_DIM * 4] = fc2_weight_amax
+
+            # Part 3 -- 8:
+            #   Int8 gemm deQFactor list (8 values): Q_deQ_scale, K_deQ_scale, V_deQ_scale, bmm1_deQ_scale, bmm2_deQ_scale,
+            #   FC0_deQ_scale, FC1_deQ_scale, FC2_deQ_scale
+            p3_offset_    = ACTIVATION_AMAX_NUM + 9 * 768
+            
+            # Q_deQ_scale
+            q_deq_scale = q_mm.outputs[0].outputs[0].inputs[1].values
+            idx = 0
+            scale_list[p3_offset_ + idx] = q_deq_scale
+
+            # K_deQ_scale
+            k_deq_scale = k_mm.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = k_deq_scale
+
+            # V_deQ_scale
+            v_deq_scale = v_mm.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = v_deq_scale
+
+            # bmm1_deQ_scale
+            bmm1_deq_scale = bmm1.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = bmm1_deq_scale
+
+            # bmm2_deQ_scale
+            bmm2_deq_scale = bmm2.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = bmm2_deq_scale
+
+            # FC0_deQ_scale
+            fc1_mm_deq_scale = fc1_mm.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = fc1_mm_deq_scale
+
+            # FC1_deQ_scale
+            fc2_mm_deq_scale = fc2_mm.outputs[0].outputs[0].inputs[1].values
+            idx += 1
+            scale_list[p3_offset_ + idx] = fc2_mm_deq_scale
+            
+            # FC2_deQ_scale = ?
+            
+
+            # Part 4 -- 3:
+            #   Amax used in trt fused mha kernel (3 values) : QKVbias_amax, Softmax_amax, bmm2_amax
+            p4_offset_    = ACTIVATION_AMAX_NUM + 9 * 768 + INT8O_GEMM_NUM
+            QKVbias_amax = np.max([q_bias_amax, k_bias_amax, v_bias_amax])
+            scale_list[p4_offset_] = QKVbias_amax
+            scale_list[p4_offset_ + 1] = softmax_amax
+            scale_list[p4_offset_ + 2] = bmm2_amax
+
+            # Part 5 -- 21: reverse
             layer_num += 1
-            continue
+            print("Export {}".format(name))
+            rst[name] = scale_list
     return rst
 
 if __name__ == "__main__":
@@ -61,7 +257,7 @@ if __name__ == "__main__":
     for node in nodes:
         name = node.name
         nodes_dict.update({name: node})
-    weights=dump_weights(nodes, nodes_dict)
+    weights=dump_scales(nodes, nodes_dict)
     
     conf = configparser.ConfigParser()
     conf.add_section("ernie")
