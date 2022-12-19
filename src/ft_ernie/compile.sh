a9274ab78f7ae35fffd4edcd631aa37069629eac
Grifcc <grifcc@foxmail.com>
2022-12-19 13:33:46 +0800

    update

--- src/ft_ernie/compile.sh
+++ src/ft_ernie/compile.sh
@@ -1,6 +1,6 @@
 CUR_DIR=`pwd`
 
-rm -rf build
+# rm -rf build
 mkdir -p build
 
 STI2_ROOT=${CUR_DIR}/../..
@@ -8,7 +8,7 @@ INSTALL_PREFIX=${STI2_ROOT}/install
 
 cd ${CUR_DIR}/build
 cmake \
-	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_VERBOSE_MAKEFILE=OFF \
 	-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
 	-DBUILD_TF=OFF \
